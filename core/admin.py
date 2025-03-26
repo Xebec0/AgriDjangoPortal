5f938bb5ca9bacd38427b9100b6fa906e3b3043d
Xebec0 <163788300+Xebec0@users.noreply.github.com>
2025-03-26 13:21:09 +0800

    Test

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Profile, AgricultureProgram, Registration
+from .models import AgricultureProgram, Profile, Registration, University, Candidate
+
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
@@ -29,3 +30,43 @@ class RegistrationAdmin(admin.ModelAdmin):
     def reject_registrations(self, request, queryset):
         queryset.update(status=Registration.REJECTED)
     reject_registrations.short_description = "Reject selected registrations"
+
+@admin.register(University)
+class UniversityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'code', 'country')
+    list_filter = ('country',)
+    search_fields = ('name', 'code', 'country')
+
+@admin.register(Candidate)
+class CandidateAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'passport_number', 'university', 'status')
+    list_filter = ('status', 'university', 'nationality', 'gender')
+    search_fields = ('first_name', 'last_name', 'passport_number', 'email')
+    date_hierarchy = 'created_at'
+    fieldsets = (
+        ('Basic Information', {
+            'fields': ('passport_number', 'first_name', 'last_name', 'email')
+        }),
+        ('Personal Details', {
+            'fields': ('date_of_birth', 'country_of_birth', 'nationality', 'religion', 'gender')
+        }),
+        ('Family Information', {
+            'fields': ('father_name', 'mother_name')
+        }),
+        ('Passport Details', {
+            'fields': ('passport_issue_date', 'passport_expiry_date', 'passport_scan')
+        }),
+        ('Physical Details', {
+            'fields': ('shoes_size', 'shirt_size', 'smokes')
+        }),
+        ('Education', {
+            'fields': ('university', 'specialization', 'secondary_specialization')
+        }),
+        ('Documents', {
+            'fields': ('terms_and_conditions', 'health_statement_menora', 'health_statement_ayalon', 
+                      'medical_report', 'info_and_rights')
+        }),
+        ('Status', {
+            'fields': ('status', 'created_by')
+        }),
+    )
